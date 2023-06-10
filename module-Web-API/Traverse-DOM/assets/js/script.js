bb2a812a8138b4edaf2686a6f049f2f287de6439
Shanzzz <shahnazafreen9849@gmail.com>
2023-06-10 10:33:15 -0400

    activities

---
+++ module-Web-API/Traverse-DOM/assets/js/script.js
@@ -0,0 +1,7 @@
+//Access element using id
+var articlesDev = document.getElementById('articles');
+var headerDiv = document.getElementById('header');
+
+// Change style by accessing style object's properties
+articlesDev.children[0].style.fontsize ='50px';
+headerDiv.children[0].style.color = 'white';
